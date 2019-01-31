57218ebd877628210c8c5559821df06dabd38ecc
xgerhard <xgerhard@2g.be>
2019-01-31 22:21:36 +0100

    Initial commit

---
+++ app/Http/Controllers/TwitchController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TwitchController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function followAge()
+    {
+        echo 'followage';
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'twitch'], function()
+{
+    $strNameRegex = '([$A-z0-9]{1,50})';
+
+    Route::get('/', function() { echo 'Twitch'; });
+
+    Route::get('followage/{channel?}/{user?}', 'TwitchController@followAge')
+        ->where('channel', $strNameRegex)
+        ->where('user', $strNameRegex);
+});
