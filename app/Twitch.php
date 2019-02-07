7bb234ec5ae0a6d5aa757d781b0157ffe7e7b2be
Gerhard <xgerhard@2g.be>
2019-02-07 14:57:00 +0100

    Refactor user search

--- app/Twitch.php
+++ app/Twitch.php
@@ -8,6 +8,8 @@ use Cache;
 
 class Twitch
 {
+    private $userCacheLength = 86400;
+
     public function __construct()
     {
         $this->twitchAPI = new TwitchAPI;
@@ -18,47 +20,54 @@ class Twitch
         $aReturnUsers = [];
         if(!empty($aUsers))
         {
-            // Check if Id is already sent by Nightbot headers
             $aNightbotUser = $oNightbot->getUser();
-            $aNighbotChannel = $oNightbot->getChannel();
-
-            foreach($aUsers as $i => $strUser)
+            if($aNightbotUser)
             {
-                // Grab Nightbot user data if available
-                if($aNightbotUser && (($i == 1 && !isset($aUsers[1])) || $aNightbotUser['name'] == $strUser))
+                if($aNightbotUser['provider'] == 'twitch')
                 {
-                    $aReturnUsers[$i] = (object) [
+                    $oUser = (object) [
                         'id' => $aNightbotUser['providerId'],
                         'name' => $aNightbotUser['name'],
                         'displayName' => $aNightbotUser['displayName']
                     ];
-                    continue;
-                }
+                    Cache::put('twitch-users-'. strtolower($oUser->name), $oUser, $this->userCacheLength);
 
-                // Grab Nightbot channel data if available
-                if($aNighbotChannel && (($i == 0 && !isset($aUsers[0])) || $aNighbotChannel['name'] == $strUser))
+                    // Set user by Nightbot headers if the user parameter is Null ($aUsers[1])
+                    if(array_key_exists(1, $aUsers) && (!$aUsers[1] || $aUsers[1] == $oUser->name))
+                        $aReturnUsers[1] = $oUser;
+                }
+                // In case the user isn't from Twitch, we can still search his username, they might be the same on the other platform
+                elseif(array_key_exists(1, $aUsers) && !$aUsers[1])
+                    $aUsers[1] = $aNightbotUser['displayName'];
+            }
+
+            $aNighbotChannel = $oNightbot->getChannel();
+            if($aNighbotChannel && $aNighbotChannel['provider'] == 'twitch')
+            {
+                $oUser = (object) [
+                    'id' => $aNighbotChannel['providerId'],
+                    'name' => $aNighbotChannel['name'],
+                    'displayName' => $aNighbotChannel['displayName']
+                ];
+                Cache::put('twitch-users-'. strtolower($oUser->name), $oUser, $this->userCacheLength);
+
+                // Set channel by Nightbot headers if the channel parameter is Null ($aUsers[0])
+                if(array_key_exists(0, $aUsers) && (!$aUsers[0] || $aUsers[0] == $oUser->name))
+                    $aReturnUsers[0] = $oUser;
+            }
+
+            foreach($aUsers as $i => $strUser)
+            {
+                if($strUser)
                 {
-                    $aReturnUsers[$i] = (object) [
-                        'id' => $aNighbotChannel['providerId'],
-                        'name' => $aNighbotChannel['name'],
-                        'displayName' => $aNighbotChannel['displayName']
-                    ];
-                    continue;
+                    // Check if user exist in local cache
+                    // If not found, add it to array that we will request to Twitch
+                    $strCacheKey = 'twitch-users-'. strtolower($strUser);
+                    if(cache::has($strCacheKey))
+                        $aReturnUsers[$i] = cache::get($strCacheKey);
+                    else
+                        $aSearchUsers[$i] = $strUser;
                 }
-
-                // Check if user exist in local cache
-                $strCacheKey = 'twitch-users-'. strtolower($strUser);
-                if(cache::has($strCacheKey))
-                {
-                    $aReturnUsers[$i] = cache::get($strCacheKey);
-                    continue;
-                }
-
-                // Can't search for null
-                if(!$strUser) throw new Exception('Channel or user parameter missing');
-
-                // If not found, add it to array that we will request to Twitch
-                $aSearchUsers[$i] = $strUser;
             }
         }
 
@@ -76,7 +85,7 @@ class Twitch
                         'displayName' => $oUser->display_name
                     ];
 
-                    Cache::put('twitch-users-'. strtolower($oUser->name), $aReturnUsers[$i], 86400);
+                    Cache::put('twitch-users-'. strtolower($oUser->name), $aReturnUsers[$i], $this->userCacheLength);
                     unset($aSearchUsers[$i]);
                 }
             }
@@ -84,7 +93,11 @@ class Twitch
 
         // We unset the searchUsers when found, if the array isn't empty yet, it doesn't exist
         if(!empty($aSearchUsers))
-            throw new Exception('User'. (count($aSearchUsers) == 1 ? '' : 's') .' not found:'. implode(', ', $aSearchUsers));
+            throw new Exception('User'. (count($aSearchUsers) == 1 ? '' : 's') .' not found: '. implode(', ', $aSearchUsers));
+
+        // If we failed to find the correct user or channel, throw exception
+        if(count($aUsers) > count($aReturnUsers))
+             throw new Exception('Channel or user parameter missing');
 
         return $aReturnUsers;
     }
