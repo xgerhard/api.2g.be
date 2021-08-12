ba2c22d048393844e879032d9d7712d859240aa8
Gerhard <xgerhard@2g.be>
2021-08-12 13:31:16 +0200

    Add GA + Steam ID finder

--- resources/views/splitgate/info.blade.php
+++ resources/views/splitgate/info.blade.php
@@ -24,8 +24,8 @@
                 </p>
                 <hr>
 
-                <div class="tab">User info:
-                    <p><input name="username" placeholder="Username / Steam ID" oninput="this.className = ''"></p>
+                <div class="tab">User info: (Find your Steam64 ID here: <a href="https://steamid.xyz" target="blank">steamid.xyz</a>)
+                    <p><input name="username" placeholder="Xbox gamertag / PSN / Steam ID" oninput="this.className = ''"></p>
                     <p>
                         <select name="platform">
                             <option value="pc">Steam</option>
@@ -270,5 +270,15 @@
                 $('#result').html(result);
             }
         </script>
+        <script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-18801404-7', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
     </body>
 </html>
